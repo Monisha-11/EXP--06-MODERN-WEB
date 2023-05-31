60e00d62552cba8442e60599a98767c60a53042a
Monisha T <93427240+Monisha-11@users.noreply.github.com>
2023-05-31 14:08:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,81 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [result, setResult] = useState('');
+
+  const handleClick = (value) => {
+    setResult(result + value);
+  };
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  const clear = () => {
+    setResult('');
+  };
+
+  return (
+    <div className="App">
+      <br></br>
+      <br></br>
+      <h2><u>SIMPLE CALCULATOR</u></h2>
+      <br></br>
+      <div className="calculator">
+        <input type="text" value={result} readOnly />
+        <div className="keypad">
+          <button onClick={() => handleClick('9')}>9</button>
+          <button onClick={() => handleClick('8')}>8</button>
+          <button onClick={() => handleClick('7')}>7</button>
+          <button onClick={() => handleClick('6')}>6</button>
+          <button onClick={() => handleClick('5')}>5</button>
+          <button onClick={() => handleClick('4')}>4</button>
+          <button onClick={() => handleClick('3')}>3</button>
+          <button onClick={() => handleClick('2')}>2</button>
+          <button onClick={() => handleClick('1')}>1</button>
+          <button onClick={() => handleClick('+')}>+</button>
+          <button onClick={() => handleClick('0')}>0</button>
+          <button onClick={() => handleClick('-')}>-</button>
+          <button onClick={() => handleClick('*')}>*</button>
+          <button onClick={clear}>C</button>
+          <button onClick={() => handleClick('/')}>/</button>
+          <button onClick={() => handleClick('.')}>.</button>
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
